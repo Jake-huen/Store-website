5ad612170459beabde7d52efafdd94ad408a87b1
김태헌 <57055730+Jake-huen@users.noreply.github.com>
2022-08-18 10:33:50 +0900

    react-hook-form을 이용하여 로그인 폼 변경

--- storewebsite/pages/login.tsx
+++ storewebsite/pages/login.tsx
@@ -1,68 +1,18 @@
 import React from 'react';
 import styled from 'styled-components';
-const Container = styled.div`
-    padding:0;
-    font-size: 14px;
-    font-family: 'Roboto', sans-serif;
-    width: 400px;
-    height: 350px;
-    padding: 40px;
-    box-sizing: border-box;
-    margin:0 auto;
-    align-items: center;
-    justify-content: center;
-`;
-const Text = styled.h2`
-    font-size: 24px;
-    color: #8b00ff;
-    margin-bottom: 20px;
-`;
-const Form = styled.form`
-    margin:0 auto;
-`;
-const Idinput = styled.input`
-    width: 100%;
-    height: 48px;
-    padding: 0 10px;
-    box-sizing: border-box;
-    margin-bottom: 16px;
-    border-radius: 6px;
-    background-color: #F8F8F8;
-`;
-const Pwdinput = styled.input`
-    width: 100%;
-    height: 48px;
-    padding: 0 10px;
-    box-sizing: border-box;
-    margin-bottom: 16px;
-    border-radius: 6px;
-    background-color: #F8F8F8;
-`;
-const Submitbutton = styled.button`
-    width: 100%;
-    color: #fff;
-    font-size: 16px;
-    background-color: #8b00ff;
-    margin-top: 20px;
-    cursor: pointer;
-    height: 40px;
-    border-radius: 10px;
-`;
+import {useForm} from 'react-hook-form';
+
 const Login = () => {
-    const submitHandler = () =>{
-        
-    }
+    const {register,handleSubmit} = useForm();
+
     return (
-        <Container>
-            <Form onSubmit={submitHandler}>
-                <Text>
-                    로그인
-                </Text>
-                <Idinput type="text" name="userId" placeholder='이메일'/>
-                <Pwdinput type="password" name="userPwd" placeholder='비밀번호'/>
-                <Submitbutton type="submit">로그인</Submitbutton>
-            </Form>
-        </Container>
+        <form onSubmit={handleSubmit((data)=>console.log(JSON.stringify(data)))}>
+            <label htmlFor='email'>이메일</label>
+            <input id="email" type="email" placeholder="이메일을 입력하세요" {...register("email")} />
+            <label htmlFor="password">비밀번호</label>
+            <input id="password" type="password" placeholder="" {...register("password")} />
+            <button type="submit">로그인</button>
+        </form>
         
     );
 };
